8c411fbbc4122a2c77d7e56b1a609d7119daccd3
angelikiKS <s212881@student.dtu.dk>
2022-03-28 09:54:39 +0000

    create module 1 : peptide

--- R/app_server.R
+++ R/app_server.R
@@ -7,4 +7,5 @@
 #' @noRd
 app_server <- function(input, output, session) {
   mod_plot_server("plot_1")
+  mod_make_peptide_server("make_peptide_1")
 }
---
+++ R/mod_make_peptide.R
@@ -0,0 +1,36 @@
+#' make_peptide UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_make_peptide_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    fluidRow(
+      column(8, "DNA_sequence"),
+      column(4, "random_dna_length", "generate_dna_button")
+    ),
+    "peptide_sequence"
+
+  )
+}
+
+#' make_peptide Server Functions
+#'
+#' @noRd
+mod_make_peptide_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+
+  })
+}
+
+## To be copied in the UI
+# mod_make_peptide_ui("make_peptide_1")
+
+## To be copied in the server
+# mod_make_peptide_server("make_peptide_1")
