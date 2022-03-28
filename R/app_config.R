fc74c403c0df7a5d9a660f60ef17a0bc19d5d3ff
bioniel <dounniel@icloud.com>
2022-03-28 08:02:21 +0000

    Initial commit

--- R/app_config.R
+++ R/app_config.R
@@ -9,7 +9,7 @@
 #'
 #' @noRd
 app_sys <- function(...) {
-  system.file(..., package = "Rbosome")
+  system.file(..., package = "rbosome")
 }
 
 
--- dev/01_start.R
+++ dev/01_start.R
@@ -19,13 +19,13 @@
 ## to change the name in the app_sys() function in app_config.R /!\
 ##
 golem::fill_desc(
-  pkg_name = "myshinyapp", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional)
+  pkg_name = "rbosome", # The Name of the package containing the App
+  pkg_title = "Centrol dogma of Molecular Biology", # The Title of the package containing the App
+  pkg_description = "Processing of the central dogma.", # The Description of the package containing the App
+  author_first_name = "Daniel", # Your First Name
+  author_last_name = "Pedrosa Martin", # Your Last Name
+  author_email = "s213270@student.dtu.dk", # Your Email
+  repo_url = "https://github.com/rforbiodatascience22/group_3_shiny" # The URL of the GitHub Repo (optional)
 )
 
 ## Set {golem} options ----
@@ -33,11 +33,11 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license("group3") # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_code_of_conduct(contact = "group3")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
