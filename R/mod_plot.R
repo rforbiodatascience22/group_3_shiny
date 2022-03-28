628ecd57534482b863d4e02ca8ade3b15d50658e
madscort <madsn@msn.com>
2022-03-28 09:13:31 +0000

    added output

--- R/mod_plot.R
+++ R/mod_plot.R
@@ -9,21 +9,24 @@
 #' @importFrom shiny NS tagList
 mod_plot_ui <- function(id){
   ns <- NS(id)
-  tagList(sidebarLayout(
-    sidebarPanel(
-      textAreaInput(
-        inputId = ns("peptide"),
-        label = "Peptide sequence",
-        width = 300,
-        height = 100,
-        placeholder = "Insert peptide sequence"
-      )
-    ),
-    mainPanel(
-      "plot"
-    )
-  )
+  tagList(
+    shiny::sidebarLayout(
+      shiny::sidebarPanel(
+        shiny::textAreaInput(
+          inputId = ns("peptide"),
+          label = "Peptide sequence",
+          width = 300,
+          height = 100,
+          placeholder = "Insert peptide sequence"
+        )
+      ),
+      shiny::mainPanel(
+        shiny::plotOutput(
+          outputId = ns("abundance")
+        )
 
+      )
+    )
   )
 }
 
