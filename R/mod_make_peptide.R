80606e8e265ca61b5ac418021c8a3ea686b319b9
madscort <madsn@msn.com>
2022-03-29 12:47:36 +0000

    Added ui

--- R/mod_make_peptide.R
+++ R/mod_make_peptide.R
@@ -4,28 +4,52 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_make_peptide_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    fluidPage(
+      fluidRow(
+        column(8,
+               "DNA_sequence",
+               uiOutput(ns("DNA_sequence"))
+               ),
+        column(4,
+               numericInput(
+                 inputId = ns("dna_length"),
+                 value = 6000,
+                 min = 3,
+                 max = 100000,
+                 step = 3,
+                 label = "Random DNA length"
+               ),
+               actionButton(
+                 inputId = ns("generate_dna"),
+                 label = "Generate", style = "margin-top: 18px;"
+                )
+               )
+      ),
+      "peptide_sequence",
+      verbatimTextOutput(outputId = ns("peptide")) %>%
+        tagAppendAttributes(style = "white-space: pre-wrap;")
+    )
   )
 }
-    
+
 #' make_peptide Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_make_peptide_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_make_peptide_ui("make_peptide_1")
-    
+
 ## To be copied in the server
 # mod_make_peptide_server("make_peptide_1")
